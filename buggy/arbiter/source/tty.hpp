28e796e8cf81aaefc89447721cf975feae6d693d
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-08-23 13:43:09 +0200

    Add software flow control

--- buggy/arbiter/source/tty.hpp
+++ buggy/arbiter/source/tty.hpp
@@ -24,6 +24,7 @@ class Tty {
                 throw std::logic_error("getting the terminal options failed");
             }
             cfmakeraw(&options);
+            options.c_iflag |= (IXON | IXOFF);
             cfsetispeed(&options, baudrate);
             cfsetospeed(&options, baudrate);
             options.c_cc[VMIN] = 0;
