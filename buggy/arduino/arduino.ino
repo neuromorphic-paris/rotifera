cc7fff0b45e01dde8249547065563c11e234a0f0
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-09-05 14:37:37 +0200

    Change pins

--- buggy/arduino/arduino.ino
+++ buggy/arduino/arduino.ino
@@ -45,8 +45,8 @@ void throttleInterruptCallback() {
 Output outputs[] = {
     {22, 1500}, // direction
     {24, 1552}, // throttle
-    {18, 1500}, // pan
-    {20, 1500}, // tilt
+    {26, 1500}, // pan
+    {28, 1500}, // tilt
 };
 
 /// Declare the read state machine.
